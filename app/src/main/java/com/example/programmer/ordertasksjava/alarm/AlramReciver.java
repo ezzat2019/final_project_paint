c56cda43953e6dd33dce8db51820232e953e48c9
ezzat2019 <coezz@yahoo.com>
2020-02-18 11:04:06 +0200

    task 1

--- app/src/main/java/com/example/programmer/ordertasksjava/alarm/AlramReciver.java
+++ app/src/main/java/com/example/programmer/ordertasksjava/alarm/AlramReciver.java
@@ -13,8 +13,8 @@ public class AlramReciver extends BroadcastReceiver {
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
+
+
         Log.d("eeeeeeeeeeeee","ezzat");
 
         Intent intent1=new Intent(context,AlarmIntentService.class);
